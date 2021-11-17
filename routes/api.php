fdd45ed8ae7c8e49741b827f59f43727e82e3ae6
Halomoan K <halomoan@gmail.com>
2021-11-17 23:04:34 +0800

    Add sanctum

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware('auth:sanctum')->group(function(){
+    Route::group([
+        'prefix' => 'manage',
+        'as' => 'manage'
+    ], function(){
+        //admin Route
+    });
+
+});
